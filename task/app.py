405d05a6aa8f512e09f7fc19ed11db5516e0c2ad
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-09-05 15:50:06 +0300

    Initial commit

--- task/app.py
+++ task/app.py
@@ -7,117 +7,34 @@ from task.models.message import Message
 from task.models.role import Role
 
 
-SYSTEM_PROMPT = """You are a RAG-powered assistant that assists users with their questions about microwave usage.
-            
-## Structure of User message:
-`RAG CONTEXT` - Retrieved documents relevant to the query.
-`USER QUESTION` - The user's actual question.
-
-## Instructions:
-- Use information from `RAG CONTEXT` as context when answering the `USER QUESTION`.
-- Cite specific sources when using information from the context.
-- Answer ONLY based on conversation history and RAG context.
-- If no relevant information exists in `RAG CONTEXT` or conversation history, state that you cannot answer the question.
+#TODO:
+# Create system prompt with info that it is RAG powered assistant.
+# Explain user message structure (firstly will be provided RAG context and the user question).
+# Provide instructions that LLM should use RAG Context when answer on User Question, will restrict LLM to answer
+# questions that are not related microwave usage, not related to context or out of history scope
+SYSTEM_PROMPT = """
 """
 
-USER_PROMPT = """##RAG CONTEXT:
-{context}
+#TODO:
+# Provide structured system prompt, with RAG Context and User Question sections.
+USER_PROMPT = """
+"""
 
 
-##USER QUESTION: 
-{query}"""
+#TODO:
+# - create embeddings client with 'text-embedding-3-small-1' model
+# - create chat completion client
+# - create text processor, DB config: {'host': 'localhost','port': 5433,'database': 'vectordb','user': 'postgres','password': 'postgres'}
+# ---
+# Create method that will run console chat with such steps:
+# - get user input from console
+# - retrieve context
+# - perform augmentation
+# - perform generation
+# - it should run in `while` loop (since it is console chat)
 
 
-embeddings_client = DialEmbeddingsClient(
-    # TODO:
-    #  Add:
-    #  - deployment_name: 'text-embedding-3-small-1'
-    #  - api_key: API_KEY
-)
-completion_client = DialChatCompletionClient(
-    # TODO:
-    #  Add:
-    #  - deployment_name: 'gpt-4o'
-    #  - api_key: API_KEY
-)
-
-text_processor = TextProcessor(
-    embeddings_client=embeddings_client,
-    db_config={
-        'host': 'localhost',
-        'port': 5433,
-        'database': 'vectordb',
-        'user': 'postgres',
-        'password': 'postgres'
-    }
-)
-
-
-def main():
-    print("🎯 Microwave RAG Assistant")
-    print("="*100)
-
-    load_context = input("\nLoad context to VectorDB (y/n)? > ").strip()
-    if load_context.lower().strip() in ['y', 'yes']:
-        # TODO:
-        #  With `text_processor` process text file:
-        #  - file_name: 'embeddings/microwave_manual.txt'
-        #  - chunk_size: 150 (or you can experiment, usually we set it as 300)
-        #  - overlap: 40 (chars overlap from previous chunk)
-
-        print("="*100)
-
-    conversation = Conversation()
-    conversation.add_message(
-        Message(Role.SYSTEM, SYSTEM_PROMPT)
-    )
-
-    while True:
-        user_request = input("\n➡️ ").strip()
-
-        if user_request.lower().strip() in ['quit', 'exit']:
-            print("👋 Goodbye")
-            break
-
-        # Step 1: Retrieval
-        print(f"{'=' * 100}\n🔍 STEP 1: RETRIEVAL\n{'-' * 100}")
-        # TODO:
-        #  Get `context` by `user_request` via `text_processor.search()`:
-        #  - search_mode: SearchMode.COSINE_DISTANCE or SearchMode.EUCLIDIAN_DISTANCE (experiment with different)
-        #  - user_request: user_request
-        #  - top_k: 5 (limit of searched results in VectorDB), experiment with different numbers
-        #  - score_threshold: 0.5 (experiment with different numbers, 0.1 -> 0.99)
-        #  - dimensions=384
-        context = None
-
-
-        # Step 2: Augmentation
-        print(f"\n{'=' * 100}\n🔗 STEP 2: AUGMENTATION\n{'-' * 100}")
-        # TODO:
-        #  1. Make Augmentation:
-        #       - format USER_PROMPT:
-        #           - context="\n\n".join(context)
-        #           - query=user_request
-        #       - assign to `augmented_prompt`
-        #  2. Add User message with Augmented content to `conversation`
-        augmented_prompt = None
-
-        print(f"Prompt:\n{augmented_prompt}")
-
-
-        # Step 3: Generation
-        print(f"\n{'=' * 100}\n🤖 STEP 3: GENERATION\n{'-' * 100}")
-        # TODO:
-        #  1. Call `completion_client.get_completion()` with message history from conversation, and assign to `ai_message`
-        #  2. Add AI message to `conversation` history
-        print(f"✅ RESPONSE:\n{ai_message.content}")
-        print("=" * 100)
 
 # TODO:
 #  PAY ATTENTION THAT YOU NEED TO RUN Postgres DB ON THE 5433 WITH PGVECTOR EXTENSION!
 #  RUN docker-compose.yml
-#  Optional: Try to change `dimensions` parameter and check what impact it will have on the `score` while searching.
-#            (You need to change it in the `init.sql` and on the chunk generations + retrieval step)
-
-# APPLICATION ENTRY POINT
-main()
--- task/embeddings/text_processor.py
+++ task/embeddings/text_processor.py
@@ -29,131 +29,27 @@ class TextProcessor:
             password=self.db_config['password']
         )
 
-    def process_text_file(
-            self,
-            file_name: str,
-            chunk_size: int,
-            overlap: int,
-            dimensions: int,
-            truncate_table: bool = True,
-    ):
-        """
-        Load content from file, chunk it, generate embeddings, and save to DB
-        Args:
-            file_name: path to file
-            chunk_size: chunk size (min 10 chars)
-            overlap: overlap chars between chunks
-            dimensions: number of dimensions to store
-            truncate_table: truncate table if true
-        """
-
-        if chunk_size < 10:
-            raise ValueError("chunk_size must be at least 10")
-        if overlap < 0:
-            raise ValueError("overlap must be at least 0")
-        if overlap >= chunk_size:
-            raise ValueError("overlap should be lower than chunkSize")
-
-        # TODO:
-        #  1. Truncate table if truncate_table == True (call the `_truncate_table()` method)
-        #  2. Open file and get content:
-        #       a. with open(file_name, 'r', encoding='utf-8') as file:
-        #       b. assign file.read() to `content`
-        #  3. Generate text chunks from `content`, use method `chunk_text()`
-        #  4. Generate dict with indexed embeddings for generated `chunks` via `embeddings_client.get_embeddings()`
-        #     and assign them to `embeddings` variable
-
-        print(f"Processing document: {file_name}")
-        print(f"Total chunks: {len(chunks)}")
-        print(f"Total embeddings: {len(embeddings)}")
-
-        # TODO:
-        #  1. Iterate through len of `chunks` (for i in range(len(chunks)))
-        #  2. Save chunk with corresponding embedding to DB (use `_save_chunk()` method)
-
-
-    def _truncate_table(self):
-        """Truncate the vectors table"""
-        # TODO:
-        #  1. Open connection (with self._get_connection() as conn)
-        #  2. Get `cursor` (with conn.cursor() as cursor)
-        #  3. Execute query `TRUNCATE TABLE vectors` with `cursor`
-        #  4. Make connection commit
-
-    def _save_chunk(self, embedding: list[float], chunk: str, document_name: str):
-        """Save chunk with embedding to database"""
-        # TODO:
-        #  1. Need convert embeddings list[float] to string and wrap embeddings into []
-        #       - f"[{','.join(map(str, embedding))}]"
-        #       - assign to `vector_string` variable
-        #  2. Open connection to DB
-        #  3. Get `cursor`
-        #  4. Execute query:
-        #       - query: INSERT INTO vectors (document_name, text, embedding) VALUES (%s, %s, %s::vector)
-        #       - vars: (document_name, chunk, vector_string)
-        #  5. Make connection commit
+    #TODO:
+    # provide method `process_text_file` that will:
+    #   - apply file name, chunk size, overlap, dimensions and bool of the table should be truncated
+    #   - truncate table with vectors if needed
+    #   - load content from file and generate chunks (in `utils.text` present `chunk_text` that will help do that)
+    #   - generate embeddings from chunks
+    #   - save (insert) embeddings and chunks to DB
+    #       hint 1: embeddings should be saved as string list
+    #       hint 2: embeddings string list should be casted to vector ({embeddings}::vector)
 
 
 
-    def search(
-            self,
-            search_mode: SearchMode,
-            user_request: str,
-            top_k: int,
-            score_threshold: float,
-            dimensions: int
-    ) -> list[str]:
-        """
-        Performs similarity search
-        Args:
-            search_mode: Search mode (Cosine or Euclidian distance)
-            user_request: User request
-            top_k: Number of results to return
-            score_threshold: Minimum score to return (range 0.0 -> 1.0)
-            dimensions: Number of dimensions to return (has to be the same as data persisted in VectorDB)
-        """
-        if top_k < 1:
-            raise ValueError("top_k must be at least 1")
-        if score_threshold < 0 or score_threshold > 1:
-            raise ValueError("score_threshold must be in [0.0..., 0.99...] range")
 
-        # TODO:
-        #  1. Generate dict with indexed embeddings for generated `chunks` via `embeddings_client.get_embeddings()`.
-        #     With this request we convert original user request into embedding for further vector search in DB.
-        #  2. Need convert embeddings list[float] to string and wrap embeddings into []
-        #       - f"[{','.join(map(str, embedding))}]"
-        #       - assign to `vector_string` variable
+    #TODO:
+    # provide method `search` that will:
+    #   - apply search mode, user request, top k for search, min score threshold and dimensions
+    #   - generate embeddings from user request
+    #   - search in DB relevant context
+    #     hint 1: to search it in DB you need to create just regular select query
+    #     hint 2: Euclidean distance `<->`, Cosine distance `<=>`
+    #     hint 3: You need to extract `text` from `vectors` table
+    #     hint 4: You need to filter distance in WHERE clause
+    #     hint 5: To get top k use `limit`
 
-        if search_mode == SearchMode.COSINE_DISTANCE:
-            max_distance = 1.0 - score_threshold
-        else:
-            max_distance = float('inf') if score_threshold == 0 else (1.0 / score_threshold) - 1.0
-
-        retrieved_chunks = []
-        with self._get_connection() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-                # TODO:
-                #  1. Execute query:
-                #       - query: Use `_get_search_query(search_mode)` method. Please take a look at query!
-                #       - vars: (vector_string, vector_string, max_distance, top_k)
-                #  2. Fetch all results with `cursor` into `results`
-                results = None
-
-                for row in results:
-                    if search_mode == SearchMode.COSINE_DISTANCE:
-                        similarity = 1.0 - row['distance']
-                    else:
-                        similarity = 1.0 / (1.0 + row['distance'])
-
-                    print(f"---Similarity score: {similarity:.2f}---")
-                    print(f"Data: {row['text']}\n")
-                    retrieved_chunks.append(row['text'])
-
-        return retrieved_chunks
-
-    def _get_search_query(self, search_mode: SearchMode) -> str:
-        return """SELECT text, embedding {mode} %s::vector AS distance
-                  FROM vectors
-                  WHERE embedding {mode} %s::vector <= %s
-                  ORDER BY distance
-                  LIMIT %s""".format(mode='<->' if search_mode == SearchMode.EUCLIDIAN_DISTANCE else '<=>')
